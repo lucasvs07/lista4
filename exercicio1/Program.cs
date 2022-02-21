018620d5ece07dafbe4033d9f7b6d15846c31fdf
lucasvs07 <78516282+lucasvs07@users.noreply.github.com>
2022-02-21 14:20:15 -0300

    Add files via upload

---
+++ exercicio1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace questao1Lista4.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Desenhe um retângulo com asteriscos, 
+               considere que o primeiro input é a largura do retângulo, 
+               e o segundo input é a altura.
+            */
+
+            Console.WriteLine("Qual a altura do retângulo?");
+            int altura = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qual a largura do retangulo?");
+            int largura = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            for (int i = 1; i <= altura; i++)
+            {
+
+                for (int j = 1; j <= largura; j++)
+                {
+
+                    Console.Write("*");
+                        
+                }
+
+                Console.Write("\n");
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Pressione qualquer tecla para sair.");
+            Console.ReadKey();
+        }
+    }
+}
