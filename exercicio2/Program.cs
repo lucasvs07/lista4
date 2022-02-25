78761db43747565df0ba607b7c11e639e21a311a
lucasvs07 <78516282+lucasvs07@users.noreply.github.com>
2022-02-24 23:59:53 -0300

    Add files via upload

---
+++ exercicio2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace questao2Lista4.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Desenhe um retângulo com asteriscos, onde as forem linhas pares,
+               escreva "A" e as linhas ímpares escreva "B"*/
+
+            Console.WriteLine("Qual a altura do retângulo?");
+            int altura = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qual a largura do retângulo?");
+            int largura = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            for(int i = 1; i <= altura; i++)
+            {
+                for (int j =1; j <= largura; j++)
+                {
+                    if (i % 2 == 0)
+                        Console.Write("A");
+                    else
+                        Console.Write("B");
+                }
+                Console.Write("\n");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Pressione qualquer tecla para sair.");
+            Console.ReadKey();
+        }
+    }
+}
